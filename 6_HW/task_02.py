63a8cf574956f16da3dd7792d4fd7cf8aa65262f
Julia <julia.masaeva@gmail.com>
2023-05-13 19:31:01 +0600

    ДЗ по 6 семинару

---
+++ 6_HW/task_02.py
@@ -0,0 +1,18 @@
+# Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+#диапазон задаем сами
+
+n = int(input('Введите количество элементов массива: '))
+import random
+list_1 =[]
+for i in range(n): 
+    list_1.append(random.randint(-10,50))
+print(list_1)
+list_2 = []
+min = int(input('min: '))      #границы диапазона
+max = int(input('max: '))
+for i in range(len(list_1)):
+     if min <= list_1[i] <= max:
+         print(i, end=' ')
+
+
